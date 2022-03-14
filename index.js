ceb25ef42f6fe631f5e059c27444c29c203f7f97
Stephen Simmons <stephensimmons@stephens-MacBook-Pro.local>
2022-03-14 08:21:38 -0700

    commit tree d7a67c1106c269b8f4f333c3627b01961e0606a7

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(bd){
+
+    const hQ = 42;
+  
+    return Math.abs(bd - hQ);
+  }
+      
+    
+    function distanceFromHqInFeet(feet){
+  
+        return (distanceFromHqInBlocks(feet)
+        * 264);
+      }
+      
+    function distanceTravelledInFeet(start, destination){
+  return Math.abs((start - destination ) * 264)
+    }
+  function  calculatesFarePrice (start, destination){
+  
+  let lowPrice= Math.abs((distanceTravelledInFeet(start, destination) -400 )* .02)
+  
+  if(distanceTravelledInFeet(start,   
+  destination) < 400){
+  return 0;}
+  
+  else if (distanceTravelledInFeet(start,   
+    destination) > 2500 ){
+      return "cannot travel that far"
+  
+    }
+    else if (distanceTravelledInFeet(start,   
+      destination) > 2000){
+        return 25;
+  
+      }
+  
+  else if(distanceTravelledInFeet(start, destination) >= 400  ){
+    return lowPrice }
+    
+    }
